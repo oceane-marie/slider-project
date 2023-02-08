4701f37174d2177a033ff5d75139e23285cdb4e8
Oceane Feltrin <oceanefeltrin@yahoo.fr>
2023-02-08 18:16:51 +0100

    reviews project completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import data from "./data"
+import { useEffect, useState } from "react";
+import { FaQuoteRight } from "react-icons/fa";
+import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
 
 function App() {
+  const [people, setPeople] = useState(data)
+  const [index, setIndex] = useState(0)
+
+  useEffect(() => {
+    const lastIndex = people.length - 1
+    if (index < 0) {
+      setIndex(lastIndex)
+    }
+    if (index > lastIndex) {
+      setIndex(0)
+    }
+  }, [index, people])
+
+  useEffect(() => {
+    let slider = setInterval(() => {
+      setIndex(index + 1)
+    }, 5000);
+    return () => {
+      clearInterval(slider)
+    }
+  }, [index])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+     <section className="section">
+      <div className="title">
+        <h2>
+          <span>/</span>Reviews
+        </h2>
+      </div>
+
+      <div className="section-center">
+        {people.map((person, personIndex) => {
+          const { id, name, image, title, quote} = person
+          let position = 'nextSlide'
+          if (personIndex === index) {
+            position = 'activeSlide'
+          }
+          if (personIndex === index - 1 ||
+            (index === 0 && personIndex === people.length - 1)) {
+            position = 'lastSlide'
+          }
+          return (
+            <article className={position} key={id}>
+              <img src={image} alt={name} className="person-img"/>
+              <h4>{name}</h4>
+              <p className="title">{title}</p>
+              <p className="text">{quote}</p>
+              <FaQuoteRight />
+            </article>
+          )
+        })}
+        <button
+            className="prev"
+            onClick={() => setIndex(index - 1)}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <FiChevronLeft />
+        </button>
+        <button
+            className="next"
+            onClick={() => setIndex(index + 1)}
+        >
+          <FiChevronRight />
+        </button>
+      </div>
+    </section>
   );
 }
 
